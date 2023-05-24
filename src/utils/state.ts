2dfb45c1c595aafcb9f48aa8acaca824e642aca5
Milena <mi_lky@mail.ru>
2023-05-24 12:51:52 +0300

    [refactor] add comments

--- src/utils/state.ts
+++ src/utils/state.ts
@@ -1,3 +1,6 @@
+/**
+ * Sets the active index of the state
+ */
 export const setActiveIndex = <T>(
   setState: React.Dispatch<React.SetStateAction<T>>,
   index: T
@@ -5,6 +8,9 @@ export const setActiveIndex = <T>(
   setState(index);
 };
 
+/**
+ * Returns "active" class if the state is equal to the index, otherwise an empty string
+ */
 export const getActiveClass = (state: number, index: number): string => {
   return state === index ? 'active' : '';
 };
