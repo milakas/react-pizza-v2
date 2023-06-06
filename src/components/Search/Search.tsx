71b9c19039e5d167c9f487227f7df426b6f9fd2c
Milena <mi_lky@mail.ru>
2023-06-06 14:34:35 +0300

    [feat] lodash debounce

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,24 +1,35 @@
 import React, { ChangeEvent } from 'react';
+import debounce from 'lodash.debounce';
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
 
 import { SearchIconWrapper, SearchMui, StyledInputBase } from './theme';
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { useAppDispatch } from '../../redux/hooks';
 import { setSearchValue } from '../../redux/slices/filter';
 import bemCreator from '../../utils/bemCreator';
 
 const cn = bemCreator('search');
 
 const Search = () => {
+  const [value, setValue] = React.useState<string>('');
+
   const dispatch = useAppDispatch();
-  const searchValue = useAppSelector((state) => state.filter.searchValue);
   const inputRef = React.useRef<HTMLInputElement>(null);
 
+  const updateSearchValue = React.useCallback(
+    debounce((value: string) => {
+      dispatch(setSearchValue(value));
+    }, 400),
+    []
+  );
+
   const handleInputChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
-    dispatch(setSearchValue(target.value));
+    setValue(target.value);
+    updateSearchValue(target.value);
   };
 
   const cleanSearch = () => {
+    setValue('');
     dispatch(setSearchValue(''));
   };
 
@@ -41,10 +52,10 @@ const Search = () => {
         inputRef={inputRef}
         placeholder="Поиск..."
         inputProps={{ 'aria-label': 'search' }}
-        value={searchValue}
+        value={value}
         onChange={handleInputChange}
       />
-      {searchValue && (
+      {value && (
         <div className={cn('close-wrapper')}>
           <CloseIcon
             className={cn('close')}
