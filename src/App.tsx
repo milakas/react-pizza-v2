520c82c8e01ddaa5053c0c87b08a311a5f953c1d
Milena <mi_lky@mail.ru>
2023-06-15 13:57:26 +0300

    [feat] outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import Header from './components/Header';
-import Home from './pages/Home';
-import Cart from './pages/Cart';
-import NotFound from './pages/NotFound';
-import FullPizza from './pages/FullPizza';
 
 import './scss/app.scss';
 
@@ -15,12 +11,7 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/pizza/:id" element={<FullPizza />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <Outlet />
         </div>
       </div>
     </div>
